c2c6d8b8d0014958de2f1f6e3eee06ae8965866e
rear0146 <shiraishi.chihiro@uec.ac.jp>
2023-07-06 00:53:25 +0900

    add draft tool

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,65 @@
-import Image from 'next/image'
+"use client";
+import React, { useState } from "react";
 import styles from './page.module.css'
 
+const positionListAll:string[] = ['GK','AC','CF','FB','FB','CB','CB','IH','IH','WG','WG','Free','Free','Free','Free','Free']
+
+const shuffleArray = (array:string[]) => {
+  const cloneArray = [...array]
+  for (let i = cloneArray.length - 1; i >= 0; i--) {
+    let rand = Math.floor(Math.random() * (i + 1))
+    let tmpStorage = cloneArray[i]
+    cloneArray[i] = cloneArray[rand]
+    cloneArray[rand] = tmpStorage
+  }
+  return cloneArray
+}
+
 export default function Home() {
+  const [selectedPositionList, selectPosition] = useState<string[]>([]);
+  const [positionList, setPositionList] = useState(positionListAll);
+  const nextPositionLot:any = () => {
+    const nextPositionIndex = Math.floor(Math.random() * positionList.length);
+    selectPosition([...selectedPositionList, positionList[nextPositionIndex]])
+    setPositionList(positionList.filter((e,index) => index !== nextPositionIndex))
+  }
+
+  const [member, setMember] = useState('');
+  const [memberList, setMemberList] = useState<string[]>([]);
+  const [memberDraft, setMemberDraft] = useState<string[]>([]);
+  const handleMemberList:any = () => {
+    setMemberList(member.split(','))
+    setMemberDraft(member.split(','))
+  }
+  const handleMemberDraft:any = () => {
+    setMemberDraft(shuffleArray(memberList))
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div className={styles.card}>
+        <input onChange={(e) => setMember(e.target.value)} />
+        <button onClick={handleMemberList}>submit</button>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.card}>
+      <button onClick={handleMemberDraft}>draft</button>
+        <ul>
+          {memberDraft.map((m:string,index:number)=>
+            <li key={index}>{index+1}位:{m}</li>
+          )}
+        </ul>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={styles.card}>
+        <button onClick={nextPositionLot}>next position</button>
+        <p>selected positions</p>
+        {selectedPositionList.map((m:string,index:number)=>
+            <li key={index}>{index+1}人目：{m}</li>
+          )}
+          <br/>
+          <p>position list</p>
+          {positionList.map((m:string,index:number)=>
+            <li key={index}>{m}</li>
+          )}
       </div>
     </main>
   )
